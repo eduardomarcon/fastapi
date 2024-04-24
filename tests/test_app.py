eca4cb00ba23b5b3d210b87cad80e6d6a99a3cd2
eduardomarcon <eduardommarcon@gmail.com>
2024-04-24 18:00:38 -0300

    feat: updating endpoints to use the real database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,6 @@
 from http import HTTPStatus
 
-
-def test_root_deve_retornar_ok_e_ola_mundo(client):
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
+from fast_zero.schemas import UserPublic
 
 
 def test_create_user(client):
@@ -26,20 +21,18 @@ def test_create_user(client):
 
 
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get('/users')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'john',
-                'email': 'john@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -56,7 +49,7 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
